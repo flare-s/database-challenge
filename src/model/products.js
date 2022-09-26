1a57dfb9ba07702ec8b5378cee8bd82cfed71cdb
flare-s <webflarez@gmail.com>
2022-09-26 16:58:03 +0100

    Completed challenge 1

--- src/model/products.js
+++ src/model/products.js
@@ -1,1 +1,13 @@
 const db = require("../database/db.js");
+
+const list_products = db.prepare(
+  /*sql*/
+  `
+SELECT id, name, quantity_per_unit, unit_price, units_in_stock, units_on_order
+FROM products;
+`
+);
+
+const listProducts = () => list_products.all();
+
+module.exports = { listProducts };
