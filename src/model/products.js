57bbf43062babd0ed20140eddf2dff2381588216
flare-s <webflarez@gmail.com>
2022-09-26 17:36:14 +0100

    Completed challenge 2

--- src/model/products.js
+++ src/model/products.js
@@ -4,10 +4,23 @@ const list_products = db.prepare(
   /*sql*/
   `
 SELECT id, name, quantity_per_unit, unit_price, units_in_stock, units_on_order
-FROM products;
+FROM products
+`
+);
+
+const find_products = db.prepare(
+  /*sql */
+  `
+SELECT 
+ id,
+ name
+FROM products
+WHERE name LIKE  ?
 `
 );
 
 const listProducts = () => list_products.all();
 
-module.exports = { listProducts };
+const searchProducts = (text) => find_products.all("%" + text + "%");
+
+module.exports = { listProducts, searchProducts };
