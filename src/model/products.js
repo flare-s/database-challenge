4051e2e5f728d11de9fcfcc800787d6d83ddbb2c
flare-s <webflarez@gmail.com>
2022-09-26 18:15:07 +0100

    Complete challenge 5

--- src/model/products.js
+++ src/model/products.js
@@ -3,7 +3,14 @@ const db = require("../database/db.js");
 const list_products = db.prepare(
   /*sql*/
   `
-SELECT id, name, quantity_per_unit, unit_price, units_in_stock, units_on_order
+SELECT 
+id,
+name,
+quantity_per_unit, 
+unit_price, 
+units_in_stock,
+units_on_order,
+unit_price * units_in_stock AS stock_value
 FROM products
 `
 );
